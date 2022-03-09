f201a18820a3a491556176406400276a48628e46
Bruce <zxc32781056@yahoo.com.tw>
2022-03-09 15:16:07 +0800

    add kotest and mokk
    using kotest and mokk

--- app/src/test/java/com/example/reqres/DomainRepositoryUnitTest2.kt
+++ app/src/test/java/com/example/reqres/DomainRepositoryUnitTest2.kt
@@ -6,40 +6,29 @@ import User
 import com.example.reqres.database.UserDao
 import com.example.reqres.database.UserTable
 import com.example.reqres.model.*
-import io.kotest.core.spec.style.AnnotationSpec
-import org.mockito.Mock
-import org.mockito.Mockito.mock
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
 
+class DomainRepositoryUnitTest2 : FunSpec({
+    val dataRepository:IDataRepository  = mockk()
+    val db: UserDao = mockk()
+    val domainRepository: IDomainRepository =  DomainRepository(dataRepository,db)
 
-class DomainRepositoryUnitTest: AnnotationSpec() {
-    @Mock private lateinit var http:IDataRepository
-    @Mock private lateinit var db:UserDao
-    @Mock private lateinit var domainRepository:IDomainRepository
-    @Before
-    fun before() {
-        http = mock(IDataRepository::class.java)
-        db = mock(UserDao::class.java)
-        domainRepository = DomainRepository(http,db)
-    }
-
-
-    @Test
-    fun `test database  read empty data`() {
+    test("test database  read empty data") {
         val state:DataResponseState<List<UserTable>> = DataResponseState.Success(arrayListOf())
         val result = domainRepository.userListIsEmpty(state).test()
-        result.assertComplete()
+        result shouldBe result.assertComplete()
     }
 
-    @Test
-    fun `test database  read have data`() {
+    test("test database  read have data") {
         val testData:List<UserTable> = listOf(UserTable(0,"0","Wan","Tome","test","1"))
         val state:DataResponseState<List<UserTable>> = DataResponseState.Success(testData)
         val result = domainRepository.userListIsEmpty(state).test()
         result.assertResult(DataResponseState.Success(testData))
     }
 
-    @Test
-    fun `test user structure transform list UserTable`() {
+    test("test user structure transform list UserTable") {
         val listData:List<Data> = arrayListOf(Data("1","1","Wan_1",0,"Tom_1"),
             Data("2","2","Wan_2",1,"Tom_2"))
         val testUser = User(listData,0,0,Support("test","url"),0,0)
@@ -49,14 +38,15 @@ class DomainRepositoryUnitTest: AnnotationSpec() {
         result.assertResult(DataResponseState.Success(ansData))
     }
 
-    @Test
-    fun `test list UserTable transform list Information`() {
+    test("test list UserTable transform list Information") {
         val testData:List<UserTable> = listOf(UserTable(0,"0","Wan","Tome","test","1"),
             UserTable(1,"1","Wan","Tome","test","2"))
-        val result = domainRepository.toUserList(DataResponseState.Success(testData)).test()
-        val ansData:List<UserInformation> = listOf(UserInformation("0","1","test","Wan","Tome"),
-            UserInformation("1","2","test","Wan","Tome"))
-        result.assertResult(DataResponseState.Success(ansData))
+        val result = domainRepository.toUserList(DataResponseState.Success(testData)).blockingGet()
+        val ansData:List<UserInformation> = listOf(
+            UserInformation("0","1","test","Wan","Tome"),
+            UserInformation("1","2","test","Wan","Tome")
+        )
+        result shouldBe DataResponseState.Success(ansData)
     }
+})
 
-}
